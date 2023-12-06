36c1b3b60c67b9afaf4f802a7a3ef343c732bfac
Beisan24 <153118998+Beisan24@users.noreply.github.com>
2023-12-06 20:39:09 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/library/Display.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.library;
+
+/**
+ *
+ * @author BeSsO
+ */
+public interface Display {
+    public abstract String getInfo();
+    public abstract boolean inLoan();
+}
